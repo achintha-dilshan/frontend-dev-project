5a138bdef4d953a7d8c3826cd120ca3c94a3b533
ACHii <achii97d@gmail.com>
2024-11-27 08:51:14 +0530

    change default font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
       colors: {
         primary: "#6B3CC9",
         secondary: "#F28D35",
