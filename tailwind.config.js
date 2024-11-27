567a1d42bf72ab9c5dd34d86ba0cfc9f492df14d
ACHii <achii97d@gmail.com>
2024-11-27 08:03:43 +0530

    color config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#6B3CC9",
+        secondary: "#F28D35",
+        analogous: {
+          1: "#6A44F2",
+          2: "#1CBDDD",
+        },
+        triadic: "#52378C",
+        greenbg: {
+          1: "#78BF91",
+          2: "#4DCA79",
+        },
+        black: "#2F2F2F",
+        light: "#545A75",
+        subtle: "#9C9991",
+        accent: {
+          1: "#E2F2FE",
+          2: "#FFF8E0",
+        },
+        error: "#FF0335",
+        success: "#5EB30B",
+      },
+    },
   },
   plugins: [],
-}
+};
