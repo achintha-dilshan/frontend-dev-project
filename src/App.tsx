ab5563e77a5994f66a5f331c73e43194e681ec9a
ACHii <achii97d@gmail.com>
2024-11-27 19:35:12 +0530

    final

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
 import "./index.css";
+import { Footer } from "./components/Footer";
+import { Navbar } from "./components/Navbar";
+import { DevelopmentSection } from "./partials/DevelopmentSection";
+import { ConsultingSection } from "./partials/ConsultingSection";
+import { HeroSection } from "./partials/HeroSection";
+import { FAQSection } from "./partials/FAQSection";
 
 function App() {
-  return <h1 className="text-red-500">Hello World!</h1>;
+  return (
+    <div className="flex flex-col min-h-screen">
+      {/* header */}
+      <Navbar />
+
+      {/* hero section */}
+      <HeroSection />
+
+      {/* development section */}
+      <DevelopmentSection />
+
+      {/* consulting section */}
+      <ConsultingSection />
+
+      {/* faq section */}
+      <FAQSection />
+
+      {/* footer */}
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
