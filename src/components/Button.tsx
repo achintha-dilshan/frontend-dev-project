402d169b60e70c58b2df468a5f0a0b5d48848106
ACHii <achii97d@gmail.com>
2024-11-27 14:28:01 +0530

    button

---
+++ src/components/Button.tsx
@@ -0,0 +1,17 @@
+import { ComponentProps } from "react";
+
+type ButtonProps = {
+  href?: string;
+} & ComponentProps<"a">;
+
+export const Button = ({ href = "#", children, ...props }: ButtonProps) => {
+  return (
+    <a
+      href={href}
+      {...props}
+      className="font-bold uppercase text-sm text-white bg-secondary leading-none rounded px-5 py-3 tracking-widest w-fit"
+    >
+      {children}
+    </a>
+  );
+};
